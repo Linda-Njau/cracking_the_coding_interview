51dbe8ac7c6662ec7135bed141da8396af7ee9b0
Linda Njau <lindanjau21@gmail.com>
2024-02-22 14:06:50 +0300

    permutation_palindrome.py

--- permutation_palindrome.py
+++ permutation_palindrome.py
@@ -1,9 +1,26 @@
 import re
-
-def test_isPermutationsofPalindrome():
-    string = "no x in nixon"
-    result =isPermutationofPalindrome(string)
-    print (result)
+import unittest
+class Test(unittest.TestCase):
+    test_cases = [
+        ("aba", True),
+        ("aab", True),
+        ("abba", True),
+        ("aabb", True),
+        ("a-bba", True),
+        ("a-bba!", True),
+        ("Tact Coa", True),
+        ("jhsabckuj ahjsbckj", True),
+        ("Able was I ere I saw Elba", True),
+        ("So patient a nurse to nurse a patient so", False),
+        ("Random Words", False),
+        ("Not a Palindrome", False),
+        ("no x in nixon", True),
+        ("azAZ", True),
+    ]
+    
+    def test_isPermutationsofPalindrome(self):
+        for [test_string, expected] in self.test_cases:
+            assert isPermutationofPalindromeBitwise(test_string) == expected
 
 def isPermutationofPalindrome(string):
     "time complexity O(n)"
@@ -19,5 +36,22 @@ def isPermutationofPalindrome(string):
                 return False
     return True
 
-test_isPermutationsofPalindrome()
+
+def isPermutationofPalindromeBitwise(string):
+    "time complexity O(n)"
+    r = 0
+    new_string = re.sub("[^A-Za-z]", "", string).lower()
+    for char in new_string:
+        val = ord(char)
+        mask = 1 << val
+        if r & mask:
+            r &= ~mask
+        else:
+            r |= mask
+    return (r -1) & r == 0
+            
+        
+    
+if __name__ == "__main__":
+    unittest.main()
         

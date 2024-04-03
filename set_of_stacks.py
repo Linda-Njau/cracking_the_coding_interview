7cb3a40abede77f3ec4a3c512df64f7709b9cfd8
Linda Njau <lindanjau21@gmail.com>
2024-04-03 12:12:43 +0300

    popAt index

--- set_of_stacks.py
+++ set_of_stacks.py
@@ -1,13 +1,19 @@
 class SetOfStacks:
-    def __init__(self):
+    def __init__(self, capacity):
         self.stacks = []
+        self.capacity = capacity
+        
+    def get_last_stack(self):
+        if not self.stacks:
+            return None
+        return self.stacks[-1]
     
     def push(self, v):
         last = self.get_last_stack()
         if last and not last.is_full():
             last.push(v)
         else:
-            stack = Stack(capacity)
+            stack = Stack(self.capacity)
             stack.push(v)
             self.stacks.append(stack)
     
@@ -22,4 +28,53 @@ class SetOfStacks:
 
 class EmptyStackException(Exception):
     pass
+
+class Stack:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.top = None
+        self.bottom = None
+        self.size = 0
     
+    def is_full(self):
+        return self.size == capacity
+    
+    def join(self, above, below):
+        if below:
+            below.above = above
+        if above:
+            above.below = below
+    
+    def push(self, value):
+        if self.size >= self.capacity:
+            return False
+        self.size += 1
+        n = Node(value)
+        if self.size == 1:
+             self.bottom = n
+        self.join(n, self.top)
+        self.top = n
+        return True
+    
+    def pop(self):
+        t = self.top
+        self.top = self.top.below
+        self.size -= 1
+        return t.value
+
+    def is_empty(self):
+        return self.size == 0
+    
+    def remove_bottom(self):
+        b = self.bottom
+        self.bottom = self.bottom.above
+        if self.bottom:
+            self.bottom.below = None
+        self.szie -= 1
+        return b.value
+    
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.above = None
+        self.below = None

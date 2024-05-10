b750451eda32e2111186bf6f46d0f1c0baff1a87
Linda Njau <lindanjau21@gmail.com>
2024-05-10 16:38:11 +0300

    Regex filtering of space characters

--- reverse_paragraph.ml
+++ reverse_paragraph.ml
@@ -1,14 +1,21 @@
+
 let rec reverse_string s =
   let words = String.split_on_char ' ' s in
-  let reversed_words = List.map(fun word -> 
-    let len = String.length word in
-    let result = Bytes.create len in
-    for i = 0 to len - 1 do
-      Bytes.set result i word.[len - 1 - i]
-    done;
-    Bytes.to_string result
+  List.iter (fun word -> print_endline ("Original word: " ^ word)) words;  (* Print original words *)
+  let reversed_words = List.map (fun word ->
+    let pattern = Str.regexp "[^a-zA-Z0-9\\p{Punct}]" in
+    let cleaned_word = Str.global_replace pattern "" word in
+      let len = String.length cleaned_word in
+      let result = Bytes.create len in
+      for i = 0 to len - 1 do
+        Bytes.set result i cleaned_word.[len - 1 - i];
+        print_endline ("Reversing character: " ^ String.make 1 word.[len - 1 - i]);  (* Print reversed character *)
+        done;
+      Bytes.to_string result
   ) words in
+  List.iter (fun word -> print_endline ("Reversed word: " ^ word)) reversed_words;  (* Print reversed words *)
   String.concat " " reversed_words
+
 
 let reverse_lines lines =
   List.map reverse_string lines
@@ -28,7 +35,6 @@ let read_input ?(file_path="") () =
     let rec read_lines acc =
       try
         let line = input_line channel in
-        print_endline line;
         read_lines (line :: acc)
       with End_of_file ->
         close_in channel;
@@ -46,14 +52,7 @@ let process_input ?file_path () =
     | Some path -> read_input ~file_path:path ()
     | None -> read_input ()
   in
-  (* Print the lines after being accumulated in the accumulator *)
-  print_endline "Lines after being accumulated:";
-  List.iter (fun line -> print_endline line) input_lines;
-  
   let reversed_lines = reverse_lines input_lines in
-  
-  (* Print the lines after being reversed *)
-  print_endline "Lines after being reversed:";
   List.iter (fun line -> print_endline line) reversed_lines
 
 let () =

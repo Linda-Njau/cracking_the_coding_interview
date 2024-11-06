ee74ea1cc1db3897ccbce73b15234664f9e251bb
Linda Njau <lindanjau21@gmail.com>
2024-11-06 11:31:19 +0300

    removeDuplicates.py

---
+++ Arrays_&_Strings/removeDuplicates.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def removeDuplicates(nums: List[int]) -> int:
+    j = 2
+    
+    for i in range(2, len(nums)):
+        if nums[i] != nums[j-2]:
+            nums[j] = nums[i]
+            j += 1
+    return j

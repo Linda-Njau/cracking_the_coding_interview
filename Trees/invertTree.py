aa2c2492f4109380e0413761b1b6b9c18ecef006
Linda Njau <lindanjau21@gmail.com>
2024-11-20 10:40:17 +0300

    invertTree.py

---
+++ Trees/invertTree.py
@@ -0,0 +1,16 @@
+from typing import Optional
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        right = self.invertTree(root.right)
+        left = self.invertTree(root.left)
+        root.left = right
+        root.right = left
+        return root

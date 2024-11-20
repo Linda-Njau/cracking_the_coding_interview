ff603529fe01a618ec49ed3288eb6b5d1686eb46
Linda Njau <lindanjau21@gmail.com>
2024-11-20 10:50:30 +0300

    isSymmetric.py

---
+++ Trees/isSymmetric.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+
+class TreeNode: 
+    def __init__(self, val=0, left=None, right=None):
+            self.val = val
+            self.left = left
+            self.right = right
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return self.isMirror(root.left, root.right)
+    def isMirror(self, left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        return left.val == right.val and self.isMirror(left.left, right.right) and self.isMirror(left.right, right.left)

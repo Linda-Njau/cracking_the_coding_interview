2b55b0a41ad7ba3fc52d9c74c0d48be5cb4654b1
Linda Njau <lindanjau21@gmail.com>
2025-02-10 18:44:51 +0300

    levelOrder.py

---
+++ Trees/levelOrder.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from collections import deque
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            levels = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                levels.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(levels)
+        return result

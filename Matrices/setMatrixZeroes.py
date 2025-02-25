1f68058e569b21ade2de999af0ef258af753dbc9
Linda Njau <lindanjau21@gmail.com>
2025-02-25 18:48:33 +0300

    setMatrixZeroes.py - constant space complexity

--- Matrices/setMatrixZeroes.py
+++ Matrices/setMatrixZeroes.py
@@ -18,7 +18,7 @@ class Solution:
 
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
-        time complexity O(mn^2)
+        time complexity O(mn)
         space complexity O(m + n)
         """
         rows, columns = len(matrix), len(matrix[0])
@@ -34,3 +34,31 @@ class Solution:
             for j in range(columns):
                 if rows_zero[i] or columns_zero[j]:
                     matrix[i][j] = 0
+    
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        time complexity O(mn)
+        space complexity O(1)
+        """
+        rows, columns = len(matrix), len(matrix[0])
+        row_zero = any(matrix[0][j] == 0 for j in range(columns))
+        col_zero = any(matrix[i][0] == 0 for i in range(rows))
+        
+        for i in range(1, rows):
+            for j in range(1, columns):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        
+        for i in range(1,rows):
+            for j in range(1, columns):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        
+        if row_zero:
+            for j in range(columns):
+                matrix[0][j] = 0
+        
+        if col_zero:
+            for i in range(rows):
+                matrix[i][0] = 0

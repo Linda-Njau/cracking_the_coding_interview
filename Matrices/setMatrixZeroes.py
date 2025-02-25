69418e1627e81302853511ae5bfcc16e15bfc0bf
Linda Njau <lindanjau21@gmail.com>
2025-02-25 16:34:01 +0300

    setMatrixZeroes.py - improved space complexity solution

--- Matrices/setMatrixZeroes.py
+++ Matrices/setMatrixZeroes.py
@@ -15,3 +15,22 @@ class Solution:
                         matrix[i][k] = 0
                     for k in range(rows):
                         matrix[k][j] = 0 
+
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        time complexity O(mn^2)
+        space complexity O(m + n)
+        """
+        rows, columns = len(matrix), len(matrix[0])
+        rows_zero = [False] * rows
+        columns_zero = [False] * columns
+        
+        for i in range(rows):
+            for j in range(columns):
+                if matrix[i][j] == 0:
+                    rows_zero[i] = True
+                    columns_zero[j] = True
+        for i in range(rows):
+            for j in range(columns):
+                if rows_zero[i] or columns_zero[j]:
+                    matrix[i][j] = 0
